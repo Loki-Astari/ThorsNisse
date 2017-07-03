2ea9bbaacec176af8834e2d06a94b41ab3fdfbb0
Loki Astari <Loki.Astari@gmail.com>
2017-07-03 14:53:57 -0700

    Added basic Socket Utility

---
+++ src/Socket/Protocol.cpp
@@ -0,0 +1,10 @@
+#include "Protocol.h"
+
+using namespace ThorsAnvil::Socket;
+
+Protocol::Protocol(DataSocket& socket)
+    : socket(socket)
+{}
+
+Protocol::~Protocol()
+{}
---
+++ src/Socket/ProtocolSimple.h
@@ -0,0 +1,22 @@
+#ifndef THORSANVIL_SOCKET_PROTOCOL_SIMPLE_H
+#define THORSANVIL_SOCKET_PROTOCOL_SIMPLE_H
+
+#include "Protocol.h"
+
+namespace ThorsAnvil
+{
+    namespace Socket
+    {
+
+class ProtocolSimple: public Protocol
+{
+    public:
+        using Protocol::Protocol;
+        void sendMessage(std::string const& url, std::string const& message) override;
+        void recvMessage(std::string& message)                               override;
+};
+
+    }
+}
+
+#endif
