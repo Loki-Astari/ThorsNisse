52796e947847a74001f9b5c4fb203658ead4e7cd
Loki Astari <Loki.Astari@gmail.com>
2017-07-04 11:20:41 -0700

    Plumb in the basic wiring for co-routines

--- src/Nisse/ProtocolHTTPNisse.cpp
+++ src/Nisse/ProtocolHTTPNisse.cpp
@@ -6,8 +6,29 @@ using namespace ThorsAnvil::Nisse;
 HTTPHandlerAccept::HTTPHandlerAccept(NisseService& parent, LibEventBase* base, ThorsAnvil::Socket::DataSocket&& so)
     : NisseHandler(parent, base, so.getSocketId())
     , socket(std::move(so))
+    , parseHeader([parent = this](Yield& yield){parent->headerParser(yield);})
 {}
 
+/*
+        std::string                                     method;
+        std::string                                     uri;
+        std::string                                     version
+        std::map<std::string, std::vector<std::string>> headers;
+*/
 void HTTPHandlerAccept::eventActivate(LibSocketId /*sockId*/, short /*eventType*/)
 {
+    parseHeader();
+}
+
+void HTTPHandlerAccept::headerParser(Yield& yield)
+{
+    while(true)
+    {
+        try
+        {
+            yield();
+        }
+        catch(...)
+        {}
+    }
 }
--- src/Nisse/ProtocolHTTPNisse.h
+++ src/Nisse/ProtocolHTTPNisse.h
@@ -3,6 +3,8 @@
 
 #include "NisseHandler.h"
 #include <boost/coroutine/asymmetric_coroutine.hpp>
+#include <map>
+#include <vector>
 
 namespace ThorsAnvil
 {
@@ -12,10 +14,21 @@ namespace ThorsAnvil
 class HTTPHandlerAccept: public NisseHandler
 {
     private:
-        ThorsAnvil::Socket::DataSocket      socket;
+        ThorsAnvil::Socket::DataSocket                  socket;
+        std::string                                     method;
+        std::string                                     uri;
+        std::string                                     version;
+        std::map<std::string, std::vector<std::string>> headers;
+
+        using CoRoutine = boost::coroutines::asymmetric_coroutine<void>::pull_type;
+        using Yield     = boost::coroutines::asymmetric_coroutine<void>::push_type;
+
+        CoRoutine   parseHeader;
     public:
         HTTPHandlerAccept(NisseService& parent, LibEventBase* base, ThorsAnvil::Socket::DataSocket&& socket);
+        void headerParser(Yield& yield);
         virtual void eventActivate(LibSocketId sockId, short eventType) override;
+
 };
 
     }
