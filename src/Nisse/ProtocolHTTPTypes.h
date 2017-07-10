2d816c10e5ff2893884fa1778f3ae538cbecf583
Loki Astari <Loki.Astari@gmail.com>
2017-07-10 02:14:03 -0700

    Parse the path get parameters

--- src/Nisse/ProtocolHTTPTypes.h
+++ src/Nisse/ProtocolHTTPTypes.h
@@ -92,16 +92,53 @@ class URI
             : port(80)
         {
             auto find = hostAndPort.find(':');
-            if (find != std::string::npos)
-            {
-                host    = hostAndPort.substr(0, find);
-                port    = atoi(hostAndPort.substr(find).c_str());
-            }
-            else
+            if (find == std::string::npos)
             {
                 host    = hostAndPort;
             }
-            path    = std::move(pathAndQuery);
+            else
+            {
+                host    = hostAndPort.substr(0, find);
+                port    = atoi(hostAndPort.substr(find + 1).c_str());
+            }
+
+            auto nextSection = pathAndQuery.find_first_of("?&#", 0, 3);
+            if (nextSection == std::string::npos)
+            {
+                path    = std::move(pathAndQuery);
+            }
+            else
+            {
+                path    = pathAndQuery.substr(0, nextSection);
+                if (pathAndQuery[nextSection] != '#')
+                {
+                    auto querySection = nextSection;
+                    nextSection = findSection(pathAndQuery, '#', querySection);
+                    query    = pathAndQuery.substr(querySection, (nextSection - querySection));
+                }
+                fragment = pathAndQuery.substr(nextSection);
+            }
+
+            if (!query.empty())
+            {
+                std::string::size_type lastPart = 0;
+                for(auto nextPart = findSection(query, '&', 0); nextPart != lastPart; lastPart = nextPart, nextPart = findSection(query, '&', lastPart))
+                {
+                    auto split = findSection(query, '=', lastPart);
+                    split = split > nextPart ? nextPart - 1 : split;
+                    queryParam[query.substr(lastPart + 1, (split - lastPart - 1))] = query.substr(split, (nextPart - split));
+                }
+            }
+        }
+    private:
+        std::string::size_type findSection(std::string const& s, char value, std::string::size_type pos)
+        {
+            if (pos == s.size())
+            {
+                return pos;
+            }
+            auto find = s.find(value, pos + 1);
+            return find == std::string::npos ? s.size() : find;
         }
 };
 
