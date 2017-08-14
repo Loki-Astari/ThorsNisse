182db061f04f6bde20cbf149a39efb4fe7f96c64
Loki Astari <Loki.Astari@gmail.com>
2017-08-13 20:56:19 -0700

    Remove print statements

--- src/NisseSQL/ConnectionNonBlocking.cpp
+++ src/NisseSQL/ConnectionNonBlocking.cpp
@@ -20,28 +20,19 @@ class MySQLConnectionHandler: public ThorsAnvil::Nisse::NisseHandler
             : NisseHandler(service, stream.getSocketId(), EV_READ | EV_WRITE)
             , worker([&connection, &stream, &username, &password, &database, &options](Yield& yield)
                 {
-                    std::cerr << "MySQLConnectionHandler: Worker\n";
                     yield(EV_WRITE);
-                    std::cerr << "MySQLConnectionHandler: Worder: Setting Yield\n";
                     ThorsAnvil::MySQL::YieldSetter  setter(stream, [&yield](){yield(EV_READ);}, [&yield](){yield(EV_WRITE);});
-                    std::cerr << "MySQLConnectionHandler: Worder: Setting Connecting to server\n";
                     connection.doConectToServer(username, password, database, options);
-                    std::cerr << "MySQLConnectionHandler: Worder: DONE\n";
                 })
         {
-            std::cerr << "MySQLConnectionHandler: CONSTRUCTOR\n";
         }
         virtual short eventActivate(ThorsAnvil::Nisse::LibSocketId /*sockId*/, short /*eventType*/)
         {
-            std::cerr << "We Can Write To DB\n";
             if (!worker())
             {
-                std::cerr << "event Activate Finished\n";
                 dropHandler();
                 return 0;
             }
-            short x = worker.get();
-            std::cerr << "DB connection Blocked: " << (x & EV_READ ? "READ" : "NR") << " : " << (x & EV_WRITE ? "WRITE" : "NW") << "\n";
             return worker.get();
         }
 };
@@ -63,11 +54,8 @@ ConnectionNonBlocking::ConnectionNonBlocking(
     : Connection(username, password, database, options, packageReader, packageWriter)
     , stream(stream)
 {
-std::cerr << "ConnectionNonBlocking::ConnectionNonBlocking\n";
     auto& service = ThorsAnvil::Nisse::NisseService::getCurrentHandler();
-std::cerr << "ConnectionNonBlocking: Service: " << &service << "\n";
     service.transferHandler<MySQLConnectionHandler>(*this, stream, username, password, database, options);
-std::cerr << "Transfer Returned\n";
 }
 
 void ConnectionNonBlocking::doConectToServer(
@@ -77,6 +65,5 @@ void ConnectionNonBlocking::doConectToServer(
         ThorsAnvil::SQL::Options const& options
 )
 {
-    std::cerr << "doConectToServer: " << username << " : " << password << " : " << database << "\n";
     conectToServer(username, password, database, options);
 }
