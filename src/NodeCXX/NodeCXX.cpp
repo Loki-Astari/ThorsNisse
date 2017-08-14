8758abcd6c62f7f60dc93644959653c19be3b0fa
Loki Astari <Loki.Astari@gmail.com>
2017-08-14 01:03:40 -0700

    Tidy up

--- src/NodeCXX/NodeCXX.cpp
+++ src/NodeCXX/NodeCXX.cpp
@@ -14,34 +14,33 @@ namespace Nisse = ThorsAnvil::Nisse;
 namespace HTTP  = ThorsAnvil::Nisse::ProtocolHTTP;
 namespace SQL   = ThorsAnvil::SQL;
 
-SQL::Connection connection("mysqlNB://test.com", "test", "testPassword", "test");
 
-void listBeer(HTTP::Request& /*request*/, HTTP::Response& response)
-{
-    response.body << "<html>"
-                  << "<head><title>Beer List</title></head>"
-                  << "<body>"
-                  << "<h1>Beer List</h1>"
-                  << "<ol>";
-
-    SQL::Statement  listBeers(connection, "SELECT Name, Age FROM Beers");
-
-    listBeers.execute([&response](std::string const& name, int age)
-        {
-            response.body << "<li>" << name << " : " << age << "</li>";
-        }
-    );
-
-
-    response.body << "</ol></body></html>";
-}
 
 int main()
 {
     try
     {
+        SQL::Connection connection("mysqlNB://test.com", "test", "testPassword", "test");
         HTTP::Site      site;
-        site.get("/listBeer", listBeer);
+        site.get("/listBeer", [&connection](HTTP::Request& /*request*/, HTTP::Response& response)
+            {
+                response.body << "<html>"
+                              << "<head><title>Beer List</title></head>"
+                              << "<body>"
+                              << "<h1>Beer List</h1>"
+                              << "<ol>";
+
+                SQL::Statement  listBeers(connection, "SELECT Name, Age FROM Beers");
+
+                listBeers.execute([&response](std::string const& name, int age)
+                    {
+                        response.body << "<li>" << name << " : " << age << "</li>";
+                    }
+                );
+
+                response.body << "</ol></body></html>";
+            }
+        );
         site.post("/addBeer", [](HTTP::Request& /*request*/, HTTP::Response& /*response*/){std::cerr << "AddBeer\n";});
 
         HTTP::Binder    binder;
